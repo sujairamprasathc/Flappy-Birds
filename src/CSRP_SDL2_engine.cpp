bfa80737cb4b68654c26c9b4428fbb860bceb094
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-10-09 13:04:12 +0530

    Add clang-format to the project and format code

--- src/CSRP_SDL2_engine.cpp
+++ src/CSRP_SDL2_engine.cpp
@@ -2,28 +2,27 @@
 
 TTF_Font *gFont = NULL;
 
-namespace CSRP_SDL2
-{
+namespace CSRP_SDL2 {
 
-bool SDL2_Engine_init()
-{
-if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )   //Starting initiation
+bool SDL2_Engine_init() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0)  // Starting initiation
     return false;
-if( TTF_Init() != 0 )   //Initialize SDL2_ttf
+  if (TTF_Init() != 0)  // Initialize SDL2_ttf
     return false;
-if( Mix_Init( MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG ) < 6 )    //Initialize all music types
+  if (Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG) <
+      6)  // Initialize all music types
     return false;
-if ( IMG_Init( IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP ) < 4 )
+  if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP) < 4)
     return false;
 
-atexit(IMG_Quit);
-atexit(Mix_Quit);
-atexit(TTF_Quit);
-atexit(SDL_Quit);
+  atexit(IMG_Quit);
+  atexit(Mix_Quit);
+  atexit(TTF_Quit);
+  atexit(SDL_Quit);
 
-gFont = TTF_OpenFont( "../res/Font.ttf", 14 );
+  gFont = TTF_OpenFont("../res/Font.ttf", 14);
 
-return true;
+  return true;
 }
 
-} // end namespace
+}  // namespace CSRP_SDL2
